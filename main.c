a92bbfee072721fc697f169de03c1ea970b87999
JakubSoltysiak <255693@edu.p.lodz.pl>
2025-01-30 08:48:55 +0100

    02 iloczyn

--- main.c
+++ main.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
+int count(int a, int b) {
+
+    return a * b;
+
+}
+
 int main(void)
 {
 
-    int sum = 0;
-    int range;
+    printf("W pierwszym branchu: %d", count(2, 3));
 
-    printf("Please enter a number: ");
-    scanf("%d", &range);
-
-    for (int i = 0; i <= range; i++) {
-
-        printf("%d ", i);
-
-        sum += i;
-
-    }
-
-    printf("\n\n");
-    printf("Suma: %d\n", sum);
-
+    return 0;
 }

2d47be1075449095e0ad9cce47c0a1873d85ecf4
JakubSoltysiak <255693@edu.p.lodz.pl>
2025-01-30 09:01:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,21 +3,8 @@
 int main(void)
 {
 
-    int sum = 0;
-    int range;
+   printf("Hello World\n");
 
-    printf("Please enter a number: ");
-    scanf("%d", &range);
-
-    for (int i = 0; i <= range; i++) {
-
-        printf("%d ", i);
-
-        sum += i;
-
-    }
-
-    printf("\n\n");
-    printf("Suma: %d\n", sum);
+   return 0;
 
 }
